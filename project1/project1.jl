2b9da24c9b0989c7030ad8365916b8b866e4f3aa
Asef Islam <asef@cs.stanford.edu>
2023-10-17 16:15:20 -0700

    initial commit

--- project1/project1.jl
+++ project1/project1.jl
@@ -1,5 +1,7 @@
 using Graphs
 using Printf
+using CSV
+using DataFrames
 
 """
     write_gph(dag::DiGraph, idx2names, filename)
@@ -14,12 +16,94 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
+function bayesian_score_component(M, α)
+    p = sum(loggamma.(α + M))
+    p -= sum(loggamma.(α))
+    p += sum(loggamma.(sum(α,dims=2)))
+    p -= sum(loggamma.(sum(α,dims=2) + sum(M,dims=2)))
+    return p
+    end
+
+
+function bayesian_score(vars, G, D)
+    n = length(vars)
+    M = statistics(vars, G, D)
+    α = prior(vars, G)
+    return sum(bayesian_score_component(M[i], α[i]) for i in 1:n)
+end
+
+function sub2ind(siz, x)
+    k = vcat(1, cumprod(siz[1:end-1]))
+    return dot(k, x .- 1) + 1
+end
+
+function statistics(vars, G, D::Matrix{Int})
+    n = size(D, 1)
+    r = [vars[i].r for i in 1:n]
+    q = [prod([r[j] for j in inneighbors(G,i)]) for i in 1:n]
+    M = [zeros(q[i], r[i]) for i in 1:n]
+    for o in eachcol(D)
+        for i in 1:n
+            k = o[i]
+            parents = inneighbors(G,i)
+            j = 1
+            if !isempty(parents)
+                j = sub2ind(r[parents], o[parents])
+            end
+            M[i][j,k] += 1.0
+        end
+    end
+    return M
+end
+
+
+struct K2Search
+    ordering::Vector{Int} # variable ordering
+end
+
+function fit(method::K2Search, vars, D)
+    G = SimpleDiGraph(length(vars))
+    for (k,i) in enumerate(method.ordering[2:end])
+        y = bayesian_score(vars, G, D)
+        while true
+            y_best, j_best = -Inf, 0
+            for j in method.ordering[1:k]
+                if !has_edge(G, j, i)
+                    add_edge!(G, j, i)
+                    y′ = bayesian_score(vars, G, D)
+                    if y′ > y_best
+                        y_best, j_best = y′, j
+                    end
+                    rem_edge!(G, j, i)
+                end
+            end
+            if y_best > y
+                y = y_best
+                add_edge!(G, j_best, i)
+            else
+                break
+            end
+        end
+    end
+    return G
+end
+    
+
 
 function compute(infile, outfile)
 
     # WRITE YOUR CODE HERE
     # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
     # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
+
+    data = CSV.File(infile) |> DataFrame
+    show(data)
+    D = Matrix(values(data))
+    vars = names(data)
+    ordering=1:length(vars)
+    method = K2Search(ordering)
+    G = fit(method, vars, D)
+    
 
 end
 
